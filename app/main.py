96986fd6a6686ea94b1d9daaa558b028bb5b6401
CastroWill <91801761+CastroWill@users.noreply.github.com>
2026-05-11 01:38:36 +0000

    feat: configuração de CORS para integração frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.connection import engine
 from app.models.agendamento import Agendamento
@@ -11,6 +12,20 @@ app = FastAPI(
     title="API Gestão Oficina",
     description="Sistema de gerenciamento de oficina",
     version="1.0.0"
+)
+
+
+origins = [
+    "*"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(agendamento_router)
